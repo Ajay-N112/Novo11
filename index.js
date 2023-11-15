479558ca9f4418050aeb6c08eb661a819e97c304
Ajay Narayanan <ajaynarayanan112@gmail.com>
2023-11-16 00:14:43 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//  import json server
+
+const jsonserver=require('json-server')
+
+
+const cors=require('cors')
+
+
+ const server=jsonserver.create()
+
+
+
+ const middleware=jsonserver.defaults()
+
+
+
+ const router=jsonserver.router('db.json')
+
+
+ server.use(middleware)
+server.use(cors())
+server.use(router)
+ const PORT=8000
+ server.listen(PORT,()=>{
+    console.log(`___json server started at port :${PORT}_____`);
+ })
